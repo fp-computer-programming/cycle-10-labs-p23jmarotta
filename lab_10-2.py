1512443cf768f435f30854a5b57f86e2a839d4a5
JM <p23jmarotta@fairfieldprep.org>
2023-01-20 13:42:12 -0500

    lab 10-1 and 10-2... but do they work???

---
+++ lab_10-2.py
@@ -0,0 +1,13 @@
+# Creator JM 1/20/2023
+
+listy = []
+
+while True:
+    num = int(input('number pls: '))
+    if num%3 == 1:
+        continue
+    elif num == -1:
+        print(listy)
+        break
+    else:
+        listy.append(num)
